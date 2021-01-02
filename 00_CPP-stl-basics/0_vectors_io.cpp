e595b36c863442b661b312ce9f8445a4fb473a6d
amansharma2910 <amansharma2910@gmail.com>
2021-01-02 14:00:00 +0530

    Completed vectors

--- 00_CPP-stl-basics/0_vectors_io.cpp
+++ 00_CPP-stl-basics/0_vectors_io.cpp
@@ -18,6 +18,29 @@ int main(){
     for(int i = 0; i < numbers.size(); i++)
         cout <<numbers[i] <<" ";
     cout <<"]";
+
+    // declaring vectors
+    vector<int> v1 {12, 23, 34, 45, 56, 67, 78, 89}; // initializing with declaration
+    vector<int> v2; // only declaration
+    
+    // pushing back (inserting elements from the back) in the vector
+    for(vector<int>::iterator i = v1.begin()+1; i != v1.end()-2; i++)
+        v2.push_back(*i);
+    
+    // priting elements of v2
+    for(auto j: v2)
+        cout <<j <<" ";
+    
+    // printing size & max_size of v2
+    cout <<"\nSize: " <<v2.size() <<" | Max-size: " <<v2.max_size() <<endl;
+    
+    // swapping elements of vectors
+    v2.swap(v1);
+    
+    // priting updated elements of v2
+    for(auto j: v2)
+        cout <<j <<" ";
+
 }
 
 
